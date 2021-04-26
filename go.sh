1a42017fef62ce793c6335d1c943a0dded4cd994
peanuts <849421294godw@gmail.com>
2021-04-26 21:57:18 +0800

    fix script

--- go.sh
+++ go.sh
@@ -8,9 +8,11 @@ targetos=`uname | tr "[A-Z]" "[a-z]"`
 if [[ $targetos == "mingw"* ]];then
 targetos="windows"
 fi
+
+bash ./env.sh
 bash ./build.sh $targetos
 
 echo "./bin/$appName -c ./etc/config.yaml $1 $2 $3 $4"
 echo "run $appName..."
 echo 
-./bin/$appName -c ./etc/config.yaml $1 $2 $3 $4
+./bin/$appName -c ./etc/config-local.yaml $1 $2 $3 $4
