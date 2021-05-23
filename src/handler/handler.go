e14ca8747d44e3600f7f72d25cca3b9eb64528b5
peanuts <849421294godw@gmail.com>
2021-05-23 15:11:05 +0800

    refactor: optimize mount handlers code.

--- src/handler/handler.go
+++ src/handler/handler.go
@@ -9,15 +9,114 @@ import (
 	"net/http"
 )
 
+// refactor
+//func EventHandler(event string) func(c *gin.Context){
+//	return func(c *gin.Context){
+//		var (
+//			data interface{}
+//			err error
+//		)
+//		switch event {
+//		case api.EventAuth:
+//			data = &api.AuthRequest{}
+//		case api.EventLoad:
+//			data = &api.LoadRequest{}
+//		case api.EventAddFriend,api.EventDeleteFriend:
+//			data = &api.FriendRequest{}
+//		case api.EventCreateGroup, api.EventJoinGroup,api.EventLeaveGroup:
+//			data = &api.GroupRequest{}
+//		default:
+//			c.JSON(http.StatusNotFound,nil)
+//			return
+//		}
+//		err = c.BindJSON(data)
+//		if err != nil {
+//			logger.Error(fmt.Sprintf("Logic.Handler %v ",event)+api.UnmarshalJsonError,err)
+//			c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
+//			return
+//		}
+//		switch event {
+//		case api.EventAuth:
+//			user, err := api.CheckToken(data.(*api.AuthRequest).Token)
+//			if db.IsNotExistError(err) {
+//				// token expired
+//				c.JSON(http.StatusOK, api.TokenInvaildResp)
+//				return
+//			}
+//			c.JSON(http.StatusOK, api.NewSuccessResponse(user))
+//			return
+//		case api.EventLoad:
+//			user, err := model.GetUserByUID(data.(*api.LoadRequest).UID)
+//			if nil != err {
+//				if db.IsNoDocumentError(err) {
+//					c.JSON(http.StatusOK, api.ResourceNotFoundResp)
+//					return
+//				}
+//				c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
+//				return
+//			}
+//			c.JSON(http.StatusOK, api.NewSuccessResponse(struct {
+//				User    *model.User     `json:"userInfo"`
+//				Friends []*model.Friend `json:"friendList"`
+//				Rooms   []*model.Room   `json:"roomList"`
+//			}{
+//				user,
+//				nil,
+//				nil,
+//			}))
+//			return
+//		case api.EventAddFriend:
+//			err = model.AddNewFriend(data.(*api.FriendRequest).FriendA, data.(*api.FriendRequest).FriendB)
+//			if err != nil {
+//				logger.Error(api.MongoDBError,err)
+//				c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
+//				return
+//			}
+//		case api.EventDeleteFriend:
+//			err = model.DeleteFriend(data.(*api.FriendRequest).FriendA, data.(*api.FriendRequest).FriendB)
+//			if err != nil {
+//				logger.Error(api.MongoDBError,err)
+//				c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
+//				return
+//			}
+//		case api.EventCreateGroup:
+//			err = model.CreateGroup(data.(*api.GroupRequest).GroupName, data.(*api.GroupRequest).GroupID)
+//			if err != nil {
+//				logger.Error(api.MongoDBError,err)
+//				c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
+//				return
+//			}
+//		case api.EventJoinGroup:
+//			err = model.CreateGroupUser(data.(*api.GroupRequest).GroupID,data.(*api.GroupRequest).UID)
+//			if err != nil {
+//				logger.Error(api.MongoDBError,err)
+//				c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
+//				return
+//			}
+//		case api.EventLeaveGroup:
+//			err = model.DeleteGroupUser(data.(*api.GroupRequest).GroupID,data.(*api.GroupRequest).UID)
+//			if err != nil {
+//				logger.Error(api.MongoDBError,err)
+//				c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
+//				return
+//			}
+//		default:
+//			c.JSON(http.StatusNotFound,nil)
+//			return
+//		}
+//		c.JSON(http.StatusOK,api.NewSuccessResponse(nil))
+//	}
+//}
+
 func Auth(c *gin.Context) {
-	json := make(map[string]interface{})
-	err := c.BindJSON(&json)
+	aR := &api.AuthRequest{}
+	err := c.BindJSON(aR)
 	if err != nil {
-		logger.Error("json unmarshal error. err: %v", err)
+		logger.Error("Logic.Auth "+api.UnmarshalJsonError, err)
 		c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
 		return
 	}
-	user, err := api.CheckToken(json["token"].(string))
+	user, err := api.CheckToken(aR.Token)
 	if db.IsNotExistError(err) {
 		// token expired
 		c.JSON(http.StatusOK, api.TokenInvaildResp)
@@ -26,33 +125,117 @@ func Auth(c *gin.Context) {
 	c.JSON(http.StatusOK, api.NewSuccessResponse(user))
 }
 
-func LoadInitData(c *gin.Context) {
-	json := make(map[string]interface{})
-	err := c.BindJSON(&json)
+func Load(c *gin.Context) {
+	lR := &api.LoadRequest{}
+	err := c.BindJSON(lR)
 	if err != nil {
-		logger.Error("json unmarshal error. err: %v", err)
+		logger.Error(api.UnmarshalJsonError, err)
 		c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
 		return
 	}
-	uid := json["user_id"].(string)
 	// get user info
-	user, err := model.GetUserByUID(uid)
+	user, err := model.GetUserByUID(lR.UID)
 	if nil != err {
 		if db.IsNoDocumentError(err) {
 			c.JSON(http.StatusOK, api.ResourceNotFoundResp)
 			return
 		}
-		logger.Error("Logic.LoadInitData get Userinfo err: %v", err)
+		logger.Error("Logic.Load "+api.MongoDBError, err)
 		c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
 		return
 	}
 	c.JSON(http.StatusOK, api.NewSuccessResponse(struct {
-		User    *model.User     `json:"user_info"`
-		Friends []*model.Friend `json:"friends_list"`
-		Rooms   []*model.Room   `json:"room_list"`
+		User    *model.User     `json:"userInfo"`
+		Friends []*model.Friend `json:"friendList"`
+		Rooms   []*model.Room   `json:"roomList"`
 	}{
 		user,
 		nil,
 		nil,
 	}))
+}
+
+func AddFriend(c *gin.Context) {
+	fR := &api.FriendRequest{}
+	err := c.BindJSON(fR)
+	if err != nil {
+		logger.Error(api.UnmarshalJsonError, err)
+		c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
+		return
+	}
+	err = model.AddNewFriend(fR.FriendA, fR.FriendB)
+	if err != nil {
+		logger.Error(api.MongoDBError, err)
+		c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
+		return
+	}
+	c.JSON(http.StatusOK, api.NewSuccessResponse(nil))
+}
+
+func DeleteFriend(c *gin.Context) {
+	fR := &api.FriendRequest{}
+	err := c.BindJSON(fR)
+	if err != nil {
+		logger.Error(api.UnmarshalJsonError, err)
+		c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
+		return
+	}
+	err = model.DeleteFriend(fR.FriendA, fR.FriendB)
+	if err != nil {
+		logger.Error(api.MongoDBError, err)
+		c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
+		return
+	}
+	c.JSON(http.StatusOK, api.NewSuccessResponse(nil))
+}
+
+func CreateGroup(c *gin.Context) {
+	gR := &api.GroupRequest{}
+	err := c.BindJSON(gR)
+	if err != nil {
+		logger.Error(api.UnmarshalJsonError, err)
+		c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
+		return
+	}
+	err = model.CreateGroup(gR.GroupName, gR.GroupID)
+	if err != nil {
+		logger.Error(api.MongoDBError, err)
+		c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
+		return
+	}
+	c.JSON(http.StatusOK, api.NewSuccessResponse(nil))
+}
+
+func JoinGroup(c *gin.Context) {
+	gR := &api.GroupRequest{}
+	err := c.BindJSON(gR)
+	if err != nil {
+		logger.Error(api.UnmarshalJsonError, err)
+		c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
+		return
+	}
+	err = model.CreateGroupUser(gR.GroupID, gR.UID)
+	if err != nil {
+		logger.Error(api.MongoDBError, err)
+		c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
+		return
+	}
+	c.JSON(http.StatusOK, api.NewSuccessResponse(nil))
+}
+
+func LeaveGroup(c *gin.Context) {
+	gR := &api.GroupRequest{}
+	err := c.BindJSON(gR)
+	if err != nil {
+		logger.Error(api.UnmarshalJsonError, err)
+		c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
+		return
+	}
+	err = model.DeleteGroupUser(gR.GroupID, gR.UID)
+	if err != nil {
+		logger.Error(api.MongoDBError, err)
+		c.JSON(http.StatusOK, api.NewHttpInnerErrorResponse(err))
+		return
+	}
+	c.JSON(http.StatusOK, api.NewSuccessResponse(nil))
 }
--- src/server/server.go
+++ src/server/server.go
@@ -20,17 +20,24 @@ func NewServer() *Server {
 func (s *Server) Init(cfg *cfgargs.SrvConfig) {
 	gin.DefaultWriter = logger.MultiWriter(logger.DefLogger().GetLogWriters()...)
 	s.httpSrv = http.NewServer(cfg)
-	s.httpSrv.AddNodeRoute(s.GetNodeRoute()...)
+	s.MountRoute()
 	go s.httpSrv.Serve(cfg) //nolint: errcheck
 }
 
-func (s *Server) GetNodeRoute() []*http.NodeRoute {
+func (s *Server) MountRoute() {
+	path := ""
 	routers := []*http.Route{
 		// TODO: Mount routes
-		http.NewRoute(api.HTTP_METHOD_POST, "auth", handler.Auth),
-		http.NewRoute(api.HTTP_METHOD_POST, "load", handler.LoadInitData),
+		http.NewRoute(api.HTTPMethodPost, api.EventAuth, handler.Auth),
+		//http.NewRoute(api.HTTPMethodPost, api.EventAuth, handler.EventHandler(api.EventAuth)),
+		http.NewRoute(api.HTTPMethodPost, api.EventLoad, handler.Load),
+		//http.NewRoute(api.HTTPMethodPost, api.EventLoad, handler.EventHandler(api.EventLoad)),
+		http.NewRoute(api.HTTPMethodPost, api.EventAddFriend, handler.AddFriend),
+		http.NewRoute(api.HTTPMethodPost, api.EventDeleteFriend, handler.DeleteFriend),
+		http.NewRoute(api.HTTPMethodPost, api.EventCreateGroup, handler.CreateGroup),
+		http.NewRoute(api.HTTPMethodPost, api.EventJoinGroup, handler.JoinGroup),
+		http.NewRoute(api.HTTPMethodPost, api.EventLeaveGroup, handler.LeaveGroup),
 	}
-
-	node := http.NewNodeRoute("", routers...)
-	return []*http.NodeRoute{node}
+	node := http.NewNodeRoute(path, routers...)
+	s.httpSrv.AddNodeRoute(node)
 }
